0f4e22c36d6b9d32e58f8a78a237a67823721edf
Sakthianu2635 <113329996+Sakthianu2635@users.noreply.github.com>
2022-11-21 16:35:06 +0530

    Add files via upload

---
+++ Train The Model On IBM/integrate with flask (1).py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pandas
+from flask import Flask, request, jsonify, render_template
+import pickle
+import inputScript
+import warnings
+warnings.filterwarnings('ignore')
+
+import requests
+
+app = Flask(__name__)
+model = pickle.load(open('Phishing_Website.pkl','rb'))
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "wLpq84jiqdQmAoNvQ9t0JafQQsECjik-mL4VIh51s4g8"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+ans = ""   
+bns = ""   
+@app.route('/y_predict', methods=['POST','GET'])
+def y_predict():
+    url = request.form['url']
+    checkprediction = inputScript.main(url)
+    #print(checkprediction)
+    # NOTE: manually define and pass the array(s) of values to be scored in the next line
+    payload_scoring = {"input_data": [{"field": [["IPAddress","LongURL","ShortURL","@Symbol","//Redirecting","PrefixSuffix","SubDomain","SSLfinal_state","DomainLength","Favicon","Port","HTTPStoken","RequestURL","AnchorURL","LinksInScriptTags","ServerFormHandler","InfoEmail","AbnormalURL","Redirect","Onmouseover","RightClick","PopupWindow","Iframe","AgeofDomain","DNSRecord","WebTraffic","PageRank","GoogleIndex","LinksPointingToPage","StatisticalReport"
+]], "values": [[-1,-1,-1,1,-1,-1,1,1,-1,1,1,-1,1,0,0,-1,1,-1,0,1,1,1,1,1,-1,1,-1,1,-1,-1]]}]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/b82d7090-8831-4285-bede-9ac562e6d2e7/predictions?version=2022-11-21', json=payload_scoring,
+    headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    predictions=response_scoring.json()
+    print(predictions)
+    pred=predictions['predictions'][0]['values'][0][0]
+    print(pred)
+    if(pred != -1):
+        output="The Website is the Legitimate Website ... Continue!!"
+        return render_template('index.html',bns=output)
+    else:
+        output="The Website is not Legitimate... BEWARE!!"
+        return render_template('index.html',ans=output)
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    
+    data = request.get_json(force=True)
+    prediction = model.y_predict([np.array(list(data.values()))])
+
+    output=prediction[0]
+    return jsonify(output)        
+ 
+if __name__ == '__main__':
+    app.run()
+
+
+# In[ ]:
+
+
+
+
